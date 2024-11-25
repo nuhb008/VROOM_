265a810e77b05911abff580e78e009db58042ea1
Nuh Islam <nuhislam@iut-dhaka.edu>
2024-11-26 03:39:34 +0600

    added navbar

--- vroom_angular/src/app/app.config.ts
+++ vroom_angular/src/app/app.config.ts
@@ -10,8 +10,18 @@ import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
 
+import { NzSpinModule } from 'ng-zorro-antd/spin'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzLayoutModule } from 'ng-zorro-antd/layout'
+import { NzSelectModule } from 'ng-zorro-antd/select'
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker'
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker'
+import { NzUploadModule } from 'ng-zorro-antd/upload'
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideNzI18n(en_US), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+ 
 };
--- vroom_angular/src/app/app.routes.ts
+++ vroom_angular/src/app/app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { BrowserModule } from '@angular/platform-browser'
 
-export const routes: Routes = [];
+import { AppComponent } from './app.component'
+import { LoginComponent } from './auth/components/login/login.component'
+import { SignupComponent } from './auth/components/signup/signup.component'
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { NzMessageModule } from 'ng-zorro-antd/message'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { NzSpinModule } from 'ng-zorro-antd/spin'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzLayoutModule } from 'ng-zorro-antd/layout'
+import { NzSelectModule } from 'ng-zorro-antd/select'
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker'
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker'
+import { NzUploadModule } from 'ng-zorro-antd/upload'
+export const routes: Routes = [
+    { path: 'register', component: SignupComponent },
+    { path: 'login', component: LoginComponent }
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutes{};
