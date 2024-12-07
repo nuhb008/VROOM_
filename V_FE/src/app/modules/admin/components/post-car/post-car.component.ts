30b62f53bd3db54eb2b6ddf7ef36af7dc901ab6c
Nuh Islam <nuhislam@iut-dhaka.edu>
2024-12-07 16:08:35 +0600

    post car

--- V_FE/src/app/modules/admin/components/post-car/post-car.component.ts
+++ V_FE/src/app/modules/admin/components/post-car/post-car.component.ts
@@ -3,6 +3,9 @@ import { FormGroup } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { FormBuilder, Validators } from '@angular/forms'
+import { AdminService } from '../../services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-post-car',
   standalone: false,
@@ -12,21 +15,21 @@ import { FormBuilder, Validators } from '@angular/forms'
 })
 export class PostCarComponent {
 
-  isSpinning: boolean = false
   postCarForm !: FormGroup;
-  selectedFile: File | null = null
-  imagePreview: string | ArrayBuffer | null = null
-  listOfOption: Array<{ label: string; value: string }> = []
-  listOfBrands = ['Toyota', 'Honda', 'BMW', 'Mercedes', 'Audi', 'Lexus']
-  listOfType = ['Sports Car', 'Diesel', 'Crossover', 'Luxury Car']
-  listOfColor = ['Red', 'Blue', 'Brown', 'Green']
-  listOfTransmission = ['Manual', 'Automatic']
+  isSpinning: boolean = false;
+  selectedFile: File | null = null;
+  imagePreview: string | ArrayBuffer | null = null;
+  listOfOption: Array<{ label: string; value: string }> = [];
+  listOfBrands = ['Toyota', 'Honda', 'BMW', 'Mercedes', 'Audi', 'Lexus'];
+  listOfType = ['Sports Car', 'Diesel', 'Crossover', 'Luxury Car'];
+  listOfColor :string[]= ['Red', 'Blue', 'Brown', 'Green'];
+  listOfTransmission = ['Manual', 'Automatic'];
 
   constructor(
     private fb: FormBuilder,
-    // private adminService: AdminService,
-    // private message: NzMessageService,
-    // private router: Router
+    private adminService: AdminService,
+    private message: NzMessageService,
+    private router: Router
   ) {}
   ngOnInit() {
     this.postCarForm = this.fb.group({
@@ -41,8 +44,8 @@ export class PostCarComponent {
     })
   }
   postCar() {
-    this.isSpinning=true;
     console.log(this.postCarForm.value);
+    this.isSpinning=true;
 
     const formData: FormData = new FormData()
     formData.append('image', this.selectedFile as Blob)
@@ -54,6 +57,15 @@ export class PostCarComponent {
     formData.append('transmission', this.postCarForm.value.transmission)
     formData.append('description', this.postCarForm.value.description)
     formData.append('price', this.postCarForm.value.price)
+    console.log(formData);
+    this.adminService.postCar(formData).subscribe((res)=>{
+      this.isSpinning=false;
+      this.message.success("Car posted successfully",{nzDuration:5000});
+      this.router.navigateByUrl("/admin/dashboard");
+      console.log(res);
+    },error=>{
+      this.message.error("Error while posting car",{nzDuration:5000})
+    })
   }
   
     onFileSelected($event: Event) {
